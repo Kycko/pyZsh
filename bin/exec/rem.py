93be675bf66d7e02d374d8e0cf7420c2468cf6ad
Anton Samartsev <ant.samarcev@gmail.com>
2026-09-17 19:56:26 +0300

    the same for rem script

--- bin/exec/rem.py
+++ bin/exec/rem.py
@@ -1,5 +1,6 @@
-from sys import exit as SYSEXIT
-from pzexec.pkgAlias import *
+from   sys import exit as SYSEXIT
+import pzexec.output   as O
+from   pzexec.pkgAlias import *
 
 SG.dist.update({
   'abbr':'rem',
@@ -9,6 +10,14 @@ SG.tasks = {
   'arch':{'cmd':['yay','-Rs'   ],'args':'' },
   'red' :{'cmd':['dnf','remove'],'args':'s'}
   }
+
+class Help(O.Help):
+  def printZSH(self):
+    cmd = ['pacman','-Qq'] if G.isArch else ['rpm','-qa','--qf','%{NAME}\n']
+    try   : final = RF.run(cmd,'t')
+    except: final = []
+
+    for line in final: print(line)
 
 # защита от запуска модуля
 if __name__ == '__main__':
